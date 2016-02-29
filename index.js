9fe8b69b253244ee014a9ad39d9fe8df46637481
yamadapc <tacla.yamada@gmail.com>
2016-02-28 23:35:20 -0300

    Add several improvements

--- index.js
+++ index.js
@@ -2,46 +2,84 @@ var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
 var chalk = require('chalk');
+var program = require('commander');
 
-var target = process.argv[2] || './src';
-var watcher = fs.watch(target, {recursive: true});
+program
+  .usage('npm-install-watch [options] ...TARGET')
+  .version('1.0.0')
+  .parse(process.argv);
 
-watcher.on('change', function(evt, filename) {
-  console.log(chalk.gray('%s - %s'), evt, filename);
-  if (path.basename(filename).indexOf('.') === 0) return;
+program.args.forEach(function(target) {
+  var stat = fs.statSync(target);
+  createWatcher(target, stat.isDirectory());
+});
 
-  filename = path.join(target, filename);
+function createWatcher(target, isDirectory) {
+  var watcher = fs.watch(target, {recursive: true});
 
-  var contents = fs.readFileSync(filename).toString();
+  watcher.on('change', function(evt, filename) {
+    console.log(chalk.blue('%s - %s'), evt, filename);
 
-  function findAllImports(string) {
-    var ms = [];
-    var regex = /import [^ ]+ from ['"]([^'"]+)['"]/gm;
-    var m = regex.exec(string);
-
-    while (m) {
-      ms.push(m[1]);
-      m = regex.exec(string);
+    if (filename && filename.charAt(filename.length - 1) === '~') {
+      filename = filename.slice(0, -1);
     }
 
-    return ms;
+    if (path.basename(filename).indexOf('.') === 0 ||
+        path.extname(filename) !== '.js' ||
+        !filename) {
+      console.log(chalk.yellow('Ignoring event %s on %s'), evt, filename);
+      return;
+    }
+
+    if (isDirectory) filename = path.join(target, filename);
+    execute(filename);
+  });
+
+  console.log(chalk.yellow('Watching for changes in %s'), target);
+}
+
+function findAll(regex, string) {
+  var ms = [];
+  var m = regex.exec(string);
+
+  while (m) {
+    ms.push(m[1]);
+    m = regex.exec(string);
   }
 
+  return ms;
+}
+
+function findAllRequires(string) {
+  var regex = /require\(['"]([^'"]+)['"]\)/gm;
+  return findAll(regex, string);
+}
+
+function findAllImports(string) {
+  var regex = /import [^ ]+ from ['"]([^'"]+)['"]/gm;
+  return findAll(regex, string);
+}
+
+function execute(filename) {
+  var contents = fs.readFileSync(filename).toString();
+
   var imports = findAllImports(contents)
+    .concat(findAllRequires(contents))
     .filter((i) => i.charAt(0) !== '.' && i.charAt(0) !== '/')
     .map((i) => i.split('/')[0]);
 
   imports.forEach((i) => {
-    console.log(chalk.yellow('Checking if %s exists...'), i);
+    process.stdout.write(chalk.gray('Checking if ' + i + ' exists... '));
 
     try {
-      require.resolve(i);
+      process.stdout.write(
+        chalk.gray('Found ' + i + ' at ' + require.resolve(i) + '\n')
+      );
     } catch(err) {
+      process.stdout.write('\n');
       console.log(chalk.blue('Installing missing dependency %s...'), i);
-      console.log(chalk.gray('     $ npm install --save %s'), i);
-      child_process.execSync('npm install --save ' + i, {stdio: 'inherit'});
+      console.log(chalk.gray('     $ npm install %s'), i);
+      child_process.execSync('npm install ' + i, {stdio: 'inherit'});
     }
   });
-});
-
-console.log(chalk.yellow('Watching for changes in %s'), target);
+}
